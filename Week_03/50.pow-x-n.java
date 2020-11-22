dd5989e7931e9885aeb953e585d1da3507c13d9d
Jacky <jacky.android@foxmail.com>
2020-11-22 11:03:58 +0800

    Week 03 update.

---
+++ Week_03/50.pow-x-n.java
@@ -0,0 +1,69 @@
+/*
+ * @lc app=leetcode id=50 lang=java
+ *
+ * [50] Pow(x, n)
+ *
+ * https://leetcode.com/problems/powx-n/description/
+ *
+ * algorithms
+ * Medium (30.51%)
+ * Likes:    1899
+ * Dislikes: 3429
+ * Total Accepted:    556.1K
+ * Total Submissions: 1.8M
+ * Testcase Example:  '2.00000\n10'
+ *
+ * Implement pow(x, n), which calculates x raised to the power n (i.e. x^n).
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: x = 2.00000, n = 10
+ * Output: 1024.00000
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: x = 2.10000, n = 3
+ * Output: 9.26100
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: x = 2.00000, n = -2
+ * Output: 0.25000
+ * Explanation: 2^-2 = 1/2^2 = 1/4 = 0.25
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * -100.0 < x < 100.0
+ * -2^31 <= n <= 2^31-1
+ * -10^4 <= x^n <= 10^4
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    
+    public double myPow(double x, int n) {
+        long N = n;
+        return N >= 0 ? quickMul(x, N) : 1.0 / quickMul(x, -N);
+    }
+
+    public double quickMul(double x, long N) {
+        if (N == 0) {
+            return 1.0;
+        }
+        double y = quickMul(x, N / 2);
+        return N % 2 == 0 ? y * y : y * y * x;
+    }
+}
+// @lc code=end
+
