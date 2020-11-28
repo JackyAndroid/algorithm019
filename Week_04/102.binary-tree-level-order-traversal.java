39384bb30204aaa0559cc28842099511e325525e
Jacky <jacky.android@foxmail.com>
2020-11-28 17:21:03 +0800

    Week 04 update.

---
+++ Week_04/102.binary-tree-level-order-traversal.java
@@ -0,0 +1,83 @@
+/*
+ * @lc app=leetcode id=102 lang=java
+ *
+ * [102] Binary Tree Level Order Traversal
+ *
+ * https://leetcode.com/problems/binary-tree-level-order-traversal/description/
+ *
+ * algorithms
+ * Medium (55.16%)
+ * Likes:    3660
+ * Dislikes: 86
+ * Total Accepted:    698.6K
+ * Total Submissions: 1.3M
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * Given a binary tree, return the level order traversal of its nodes' values.
+ * (ie, from left to right, level by level).
+ * 
+ * 
+ * For example:
+ * Given binary tree [3,9,20,null,null,15,7],
+ * 
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ * 
+ * 
+ * 
+ * return its level order traversal as:
+ * 
+ * [
+ * ⁠ [3],
+ * ⁠ [9,20],
+ * ⁠ [15,7]
+ * ]
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if(root == null) return result;
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            List<Integer> currentLevel = new ArrayList<>();
+            for(int i = 0; i < size; i++){
+                TreeNode current = queue.remove();
+                currentLevel.add(current.val);
+                if(current.left != null)
+                    queue.add(current.left);
+                if(current.right != null)
+                    queue.add(current.right);
+            }
+            result.add(currentLevel);
+        }
+        return result;
+    }
+}
+// @lc code=end
+
